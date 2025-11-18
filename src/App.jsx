c585c1df4d7c0a5557f4015aa4c20289d1a4298e
Cinho <marciolacourcelle@gmail.com>
2025-11-18 08:24:52 +0100

    Architecture de l'app et dependances installé : création des routes en cours

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from "./pages/Home.jsx";
+import Logement from "./pages/Logement.jsx";
+import About from "./pages/About.jsx";
+import Error404 from "./pages/Error404.jsx";
+import './App.css'
+
+function App() {
+
+
+}
+
+export default App
